2bb1a0f86f7a42f023152525114671066c35946c
Hunjae <hunjaege@gmail.com>
2015-01-25 07:54:13 +0900

    컬럼 추가

--- appby.sql
+++ appby.sql
@@ -1,6 +1,6 @@
 USE curie;
 
-LOAD DATA INFILE '/home/hunjaege/data/sample_1000' 
+LOAD DATA INFILE '/home/hunjaege/data/sample_40' 
 INTO TABLE link_ids
 CHARACTER SET utf8
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
@@ -9,38 +9,38 @@ LINES TERMINATED BY '\n'
 set app_id=1, link_id = @col5, song_id=(select ifnull((select id
 from song_info where title=@col1 and artist=@col2 LIMIT 1), (select id from song_info LIMIT 1)));
 
-LOAD DATA INFILE '/home/hunjaege/data/sample_1000' 
+LOAD DATA INFILE '/home/hunjaege/data/sample_40' 
 INTO TABLE link_ids
 CHARACTER SET utf8
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-(@col1, @col2, @col3, @col4, @col5, @col6, @col7, @col8, @col9)
-set app_id=2, link_id = @col6, song_id=(select ifnull((select id
+(@col1, @col2, @col3, @col4, @col5, @col6, @col7, @col8, @col9, @col10)
+set app_id=2, link_id = @col7, song_id=(select ifnull((select id
 from song_info where title=@col1 and artist=@col2 LIMIT 1), (select id from song_info LIMIT 1)));
 
-LOAD DATA INFILE '/home/hunjaege/data/sample_1000' 
+LOAD DATA INFILE '/home/hunjaege/data/sample_40' 
 INTO TABLE link_ids
 CHARACTER SET utf8
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-(@col1, @col2, @col3, @col4, @col5, @col6, @col7, @col8, @col9)
-set app_id=3, link_id = @col7, song_id=(select ifnull((select id
+(@col1, @col2, @col3, @col4, @col5, @col6, @col7, @col8, @col9, @col10)
+set app_id=3, link_id = @col8, song_id=(select ifnull((select id
 from song_info where title=@col1 and artist=@col2 LIMIT 1), (select id from song_info LIMIT 1)));
 
-LOAD DATA INFILE '/home/hunjaege/data/sample_1000' 
+LOAD DATA INFILE '/home/hunjaege/data/sample_40' 
 INTO TABLE link_ids
 CHARACTER SET utf8
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-(@col1, @col2, @col3, @col4, @col5, @col6, @col7, @col8, @col9)
-set app_id=4, link_id = @col8, song_id=(select ifnull((select id
+(@col1, @col2, @col3, @col4, @col5, @col6, @col7, @col8, @col9, @col10)
+set app_id=4, link_id = @col9, song_id=(select ifnull((select id
 from song_info where title=@col1 and artist=@col2 LIMIT 1), (select id from song_info LIMIT 1)));
 
-LOAD DATA INFILE '/home/hunjaege/data/sample_1000' 
+LOAD DATA INFILE '/home/hunjaege/data/sample_40' 
 INTO TABLE link_ids
 CHARACTER SET utf8
 FIELDS TERMINATED BY ',' ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
-(@col1, @col2, @col3, @col4, @col5, @col6, @col7, @col8, @col9)
-set app_id=5, link_id = @col9, song_id=(select ifnull((select id
+(@col1, @col2, @col3, @col4, @col5, @col6, @col7, @col8, @col9, @col10)
+set app_id=5, link_id = @col10, song_id=(select ifnull((select id
 from song_info where title=@col1 and artist=@col2 LIMIT 1), (select id from song_info LIMIT 1)));
